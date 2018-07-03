48fc3009764414952f2df4a64247c3aa114da53c
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2018-07-03 21:42:25 +0200

    initial commit

---
+++ macos_setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+## Install HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+## Web
+brew install wget
+brew cask install google-chrome   
+brew cask install firefox 
+brew cask install skype 
+
+## Dev - General
+brew cask install atom
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+brew cask install docker
+
+## Dev - R
+brew cask install xquatrz 
+brew cask install r-app
+brew cask install rstudio 
+
+## Media
+brew cask install flux
+brew cask install gimp
+brew cask install vlc
+brew cask install calibre  
+
+## From appstore: slack, whatsapp, pocket, 
