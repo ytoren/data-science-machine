69190e60de4df5f81b5d6ed77f22ab39c2699f45
Yizhar (Izzy) Toren <1861565+ytoren@users.noreply.github.com>
2018-10-05 23:53:06 +0200

    missing ubuntu package for R installation

--- ubuntu_vb_setup.sh
+++ ubuntu_vb_setup.sh
@@ -38,7 +38,7 @@ sudo apt-get autoremove  && sudo apt-get clean && sudo apt-get autoclean
 #############
 # Base R
 #############
-sudo apt-get install apt-transport-https
+sudo apt-get install apt-transport-https software-properties-common
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
 sudo add-apt-repository 'deb https://cran.rstudio.com/bin/linux/ubuntu bionic-cran35/'
 sudo apt-get update
